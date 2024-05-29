1ca807d6efe4d19651d5763edcc4eb3207142979
charlie <2295509496@qq.com>
2024-05-29 21:49:13 +0800

    @RequestBody注解表示前端以json格式请求数据

--- src/main/java/com/charlie/furn/controller/FurnController.java
+++ src/main/java/com/charlie/furn/controller/FurnController.java
@@ -25,8 +25,12 @@ public class FurnController {
 
     // 编写方法完成添加
     // 1. 当前前端是以 json格式 来发送添加信息furn，那么需要使用 @RequestBody注解
-    // 2. 使用 @RequestBody 将客户端提交的json数据，封装成JavaBean对象
-    // 3. 如果前端是以 表单形式 提交的数据，则不需要使用
+    // 2. 使用 @RequestBody 将客户端提交的json数据，封装成JavaBean对象。
+    //          前端提交数据时Content-Type: application/json
+    // 3. 如果前端通过 表单(form-data) 的形式传入数据，则会报错：Content type 'multipart/form-data not supported
+    //      前端请求数据格式必须与 http请求头(header) Content-Type 指定的格式相同，否则会报错：Status: 500服务器错误
+    //      因为不一致导致传过来的数据信息全为空，所以服务器端报错 furn=Furn(id=null, name=null, maker=null, price=null, sales=null, stock=null)
+    // 4. 如果前端是以 表单(multipart/form-data) 形式提交的数据，则不需要使用
     @PostMapping("/save")
     public Result save(@RequestBody Furn furn) {
         log.info("furn={}", furn);
